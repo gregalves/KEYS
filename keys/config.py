85ee47f84a93fd189513445bd39f2f0001d78141
Celso Alves <celso.gregorio@gmail.com>
2024-10-04 12:23:03 +0000

    Commit inicial do sistema KEYS

---
+++ keys/config.py
@@ -0,0 +1,14 @@
+import os
+
+class Config:
+    SECRET_KEY = 'your_secret_key'
+#    SQLALCHEMY_DATABASE_URI = 'sqlite:///keys.db'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://keys_user:toor@localhost/keys_db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+# Configurações de e-mail (por exemplo, usando Gmail)
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'celso.gregorio@gmail.com'
+    MAIL_PASSWORD = 'wbun wucw eqzw zhlt'  # Ou use variáveis de ambiente para segurança
+    MAIL_DEFAULT_SENDER = ('Suporte N1 - N2', 'your_email@gmail.com')
